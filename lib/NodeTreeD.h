0fe9e37ae3249dfc140d1d5f6ea4ed256ff2ca32
estebancb03 <estebanr.castaneda03@gmail.com>
2021-10-25 10:28:18 -0600

    Creacion de la clase node para el TreeD

---
+++ lib/NodeTreeD.h
@@ -0,0 +1,22 @@
+#ifndef NODETREED_H
+#define NODETREED_H
+template < class T >
+
+class NodeTreeD {
+    T object;
+    NodeTreeD< T > *leftmostSon;
+    NodeTreeD< T > *leftBrother;
+    NodeTreeD< T > *rightBrother;
+    public:
+        NodeTreeD(T o) { object = o; leftmostSon = nullptr; rightBrother = nullptr; };
+        NodeTreeD< T > *getLeftmostSon() { return leftmostSon; };
+        NodeTreeD< T > *getLeftBrother() { return leftBrother; };
+        NodeTreeD< T > *getRightBrother() { return rightBrother; };
+        T getObject() { return object; };
+        void setObject(T o) { object = o; };
+        void setLeftmostSon(NodeTreeD< T > *ls) { leftmostSon = ls; };
+        void setLeftBrother(NodeTreeD< T > *lb) { leftBrother = lb; };
+        void setRightBrother(NodeTreeD< T > *rb) { rightBrother = rb; };
+};
+
+#endif 
