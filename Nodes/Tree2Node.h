0198ed37713af9c4947969cfb376abdfdb3bc831
estebancb03 <estebanr.castaneda03@gmail.com>
2021-10-30 00:48:02 -0600

    Node fara TreeB

--- Nodes/Tree2Node.h
+++ Nodes/Tree2Node.h
@@ -3,8 +3,13 @@
 
 template < class T >
 class Node {
+    T object;
+    Node< T > *father;
     public:
-
+        Node(T o) { object = o; next = nullptr; };
+        Node< T > *getFather() { return father; };
+        T getObject() { return object; };
+        void setFather(Node< T > *f) { father = f; };
 };
 
 #endif // TREE2NODE_H
