68a6c1125c3fbb3f8e0d6611f98152b9bd6b9b27
estebancb03 <estebanr.castaneda03@gmail.com>
2021-10-29 23:53:17 -0600

    Planteamineto de lista simplemente enlazada

--- List/List.h
+++ List/List.h
@@ -6,7 +6,85 @@ template < class T >
 class List {
     GenericNode< T > *head;
     public:
-        
+        List() { head = nullptr; };
+        bool exist(T object);
+        bool empty();
+        void destroy();
+        void deleteObject(GenericNode< T > *node);
+        void insert(T object);
+        T getObject(GenericNode< T > *node);
+        GenericNode< T > *getNode(T object);
+        GenericNode< T > *getNodeByPosition(int position);
 };
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+void List< T > :: destroy() {
+
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+bool List< T > :: empty() {
+
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+T List< T > :: getObject(GenericNode< T > *node) {
+
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+GenericNode< T >* List< T > :: getNode(T object) {
+    
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+GenericNode< T >* List< T > :: getNodeByPosition(int position) {
+
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+void List< T > :: insert(T object) {
+
+}
+
+/*
+    EFECTO:
+    REQUIERE:
+    MODIFICA:
+*/
+template < typename T >
+void List< T > :: deleteObject(GenericNode< T > *node) {
+
+}
 
 #endif //LIST_H
